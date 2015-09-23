d83279475a1aecf0162ef5566e02c4a08faaec2a
wuhy <sparklewhy@gmail.com>
2015-09-23 10:40:09 +0800

    support pass  register task param as an array

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ exports.inline = Inliner.inline;
  * 增加自定义的内联任务方法
  *
  * @param {string} type 内联的处理器类型，`html` | `js` | `css` | `img` | `font` | `svg`
- * @param {Object} task，增加的内联任务，可以是如下两种结构：
+ * @param {Object} task，增加的内联任务，可以是如下三种结构：
  *        1) 直接传入执行的任务处理方法
  *        function (file) { return file.data; }
  *        2) 传入一个对象，结构如下
@@ -30,6 +30,7 @@ exports.inline = Inliner.inline;
  *          enable: function () { return true; },
  *          task: function (file) { return file.data }
  *        }
+ *        3）传入一个数组，表示任务列表，数组元素结构为1）或 2）
  */
 exports.addInlineTaskFor = function (type, task) {
     Inliner.addInlineTask(type, task);
